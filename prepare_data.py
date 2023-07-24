1adcb09e2cff8ae3c96dd0ec9970e92f6d80cb01
linnchong <ychong@uni-bonn.de>
2023-07-24 14:49:23 +0200

    lmdb no resize only crop

--- prepare_data.py
+++ prepare_data.py
@@ -13,6 +13,9 @@ from torchvision.transforms import functional as trans_fn
 def resize_and_convert(img, size, resample, quality=100):
     # img = trans_fn.resize(img, size, resample)
     img = trans_fn.center_crop(img, size)
+    # FIXME doing that weird thing where I try to match the UAV resolution of the UGV 
+    # img = trans_fn.resize(img,size)
+
     buffer = BytesIO()
     img.save(buffer, format="jpeg", quality=quality)
     val = buffer.getvalue()
